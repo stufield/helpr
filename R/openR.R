64f60ebd0a4f59ebb8c4acaf7c8d7cdbc54dbdc1
Stu Field <stu.g.field@gmail.com>
2024-11-09 08:27:14 -0700

    Fix message in openR() and improve docs

--- R/openR.R
+++ R/openR.R
@@ -1,16 +1,17 @@
 #' Open File Interactively
 #'
 #' Opens a text file in an `RStudio` interactive session. Must be using
-#' `RStudio` interactively. Similar to `rstudioapi::navigateToFile()` but
-#' without the added dependency.
+#'   `RStudio` interactively. Similar to `rstudioapi::navigateToFile()` but
+#'   without the added \pkg{rstudioapi} dependency.
 #'
 #' @param file A path to a file.
 #' @return The file path, invisibly.
+#'
 #' @export
 openR <- function(file) {
   if ( !(identical(.Platform$GUI, "RStudio") && interactive()) ) {
     stop(
-     "Must be in an interactive RStudio session to use `openInteractive()`.",
+     "Must be in an interactive RStudio session to use `openR()`.",
       call. = FALSE
     )
   }
