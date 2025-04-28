32bb6460b142105f26f2228bb2914b6ff3d81c7b
Stu Field <stu.g.field@gmail.com>
2025-04-28 14:54:55 -0600

    Added new function `write_latex_tbl()`
    - from old code but good one to have
      in the back pocket

--- R/write-text.R
+++ R/write-text.R
@@ -8,6 +8,7 @@
 #' @inheritParams usethis::write_over
 #'
 #' @param overwrite `logical(1)`. If file exists, over-write it?
+#'
 #' @export
 write_text <- function(path, lines, overwrite = FALSE) {
   stopifnot(
@@ -32,9 +33,96 @@ write_text <- function(path, lines, overwrite = FALSE) {
 #' @describeIn write_text
 #'   a convenient wrapper to [readLines()] with default
 #'   UTF-8 encoding for reading text into an R session.
+#'
 #' @inheritParams base::readLines
 #'
 #' @export
 read_text <- function(path, n = -1L) {
   base::readLines(path, n = n, encoding = "UTF-8", warn = FALSE)
+}
+
+#' @describeIn write_text
+#'   write a data frame to a *LaTeX* table.
+#'
+#' @inheritParams rownames
+#'
+#' @param append `logical(1)`. See [write.table()].
+#'
+#' @param include_rn `logical(1)`. Should row names
+#'   of `data` be included?
+#'
+#' @param rn_label If row names are to be included,
+#'   the title for that column.
+#'
+#' @param caption A possible caption for the table in *LaTeX*.
+#'
+#' @param long `logical(1)`. Should the table be written
+#'   in long format. Better for very long tables.
+#'
+#' @param ... Arguments passed to [write.table()].
+#'
+#' @export
+write_latex_tbl <- function(data, path, append = FALSE, include_rn = TRUE,
+                            rn_label = "", caption = NULL, long = FALSE,  ...) {
+
+  stopifnot(is.data.frame(data))
+  withr::local_output_sink(path, append = append)
+
+  table <- ifelse(long, "longtable", "tabular")
+
+  data <- data |>
+    set_Names(gsub, pattern = "_", replacement = "\\_", fixed = TRUE)
+  data <- data |>
+    set_rn(gsub("_", "\\_", rownames(data), fixed = TRUE))
+
+  if ( include_rn ) {
+    cols <- paste0("l", strrep("c", ncol(data)))
+    header <- sprintf("\\hline\n\\textbf{%s} & \\textbf{%s} \\\\\n\\hline\n",
+                      rn_label, paste(names(data), collapse = "} & \\textbf{"))
+  } else {
+    cols <- paste0("l", strrep("c", ncol(data) - 1L))
+    header <- sprintf("\\hline\n\\textbf{%s} \\\\\n\\hline\n",
+                      paste(names(data), collapse = "} & \\textbf{"))
+  }
+
+  cat(sprintf("\\begin{%s}{", table), cols, "}\n", sep = "")
+
+  if ( !is.null(caption) ) {
+    sprintf("\\multicolumn{%i}{c}", nchar(cols)) |>
+      paste0("\n{\\small \\textbf{\\tablename\\ ") |>
+      paste0("\\thetable{} -- ", caption, "}} \\\\\n") |>
+      cat()
+  }
+
+  cat(header)
+
+  if ( long ) {
+    cat("\\endfirsthead\n\n")
+    sprintf("\\multicolumn{%i}{c}", nchar(cols)) |>
+      paste0("\n{{\\tablename\\ \\thetable{} -- ") |>
+      paste0("continued from previous page}} \\\\\n") |>
+      cat()
+    cat(header)
+    cat("\\endhead\n\n")
+  }
+
+  for ( i in seq_len(ncol(data)) ) {
+    if ( is.numeric(data[[i]]) )
+      data[[i]] <- format(data[[i]], nsmall = 2L, digits = 3L,
+                          scientific = abs(min(data[[i]], na.rm = TRUE)) < 0.01)
+  }
+
+  is_fct <- which(vapply(data, is.factor, NA))
+  for ( i in is_fct ) {
+    data[[i]] <- as.character(data[[i]])
+  }
+
+  is_chr <- which(vapply(data, is.character, NA))
+  for ( i in is_chr ) {
+    data[[i]] <- gsub("%", "\\\\%", data[[i]])
+  }
+
+  write.table(data, sep = " & ", quote = FALSE, col.names = FALSE,
+              row.names = include_rn, eol = "\\\\\n", ...)
+  cat(sprintf("\\hline\n\\end{%s}\n", table))
 }
